701c76bef718274773db5aaf35a659fa27d9ee76
Sol Alvarez <101482277+solealv@users.noreply.github.com>
2024-10-30 17:24:14 +0100

    Update streamlit_app.py
    testing API

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,6 +18,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 #st.dataframe(data=my_dataframe, use_container_width=True)
 pd_df=my_dataframe.to_pandas()
 #st.dataframe(pd_df)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
+st.text(fruityvice_response)
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredientes', my_dataframe, max_selections=5
@@ -30,8 +32,8 @@ if ingredients_list:
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         
         st.subheader(fruit_chosen + 'Nutrition Information')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
-        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+        #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
+        #fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
     st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
